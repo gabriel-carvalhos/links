c62eb90bd99d75771f133d093a267478f7910017
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2025-05-24 11:59:25 -0300

    add title

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <main className="bg-[#0a0a0a] text-white min-h-dvh flex justify-center items-center">
+        <div>
+          <h1 className="font-title italic leading-9 tracking-tight lowercase text-5xl">
+            Gabriel
+            <span className="block ml-4">Carvalho</span>
+          </h1>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App
--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl">Links</h1>
-    </>
-  )
-}
-
-export default App
