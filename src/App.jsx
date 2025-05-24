89851c705d2c7b89229c6226acd296f6e2ff272b
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2025-05-24 14:40:47 -0300

    add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Links from "./components/Links";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <main className="bg-[#0a0a0a] text-white min-h-dvh flex justify-center items-center">
         <div className="w-4/5 max-w-96">
-          <h1 className="font-title italic leading-9 tracking-[-3px] lowercase text-5xl w-fit mx-auto mb-6">
-            Gabriel
-            <span className="block ml-4">Carvalho</span>
-          </h1>
+          <Header />
 
           <Links />
         </div>
