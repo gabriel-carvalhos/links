7e36c9187995b1aac369d8bfa44b080f94e8affa
gabriel-carvalhos <gabrielcarvalhos.snt@gmail.com>
2025-05-25 18:21:52 -0300

    add handleTheme function

--- src/components/ThemeDropdown.jsx
+++ src/components/ThemeDropdown.jsx
@@ -1,13 +1,21 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 import { ChevronDownIcon } from "@heroicons/react/16/solid";
 import ThemeButton from "./ThemeButton";
+import { useState } from "react";
 
 function ThemeDropdown() {
+    const [theme, setTheme] = useState('dark');
+
+    const handleTheme = () => setTheme()
+
     return (
         <div className="fixed top-6 right-6 opacity-0 animate-opacity">
             <div className="dropdown dropdown-end">
                 <div tabIndex={0} role="button" className="flex gap-1 items-center bg-[#333] hover:bg-[#0a0a0a] active:bg-black focus:bg-black transition-colors rounded-md border p-2 mb-2 cursor-pointer">
-                    <MoonIcon className="size-5 text-white"/>
+                    {theme == 'dark' ? 
+                        <MoonIcon className="size-5 text-white"/> :
+                        <SunIcon className="size-5 text-white"/>
+                    }
                     <ChevronDownIcon className="size-5 text-white"/>
                 </div>
 
